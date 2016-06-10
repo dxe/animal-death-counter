905cbfc4aec59386fa5e6e8c77c8c06745fa0ab2
Alex Gleason <alex@alexgleason.me>
2016-06-10 14:41:18 -0400

    Wrap JS in DOM loaded event; run counter immediately

--- www/assets/scripts.js
+++ www/assets/scripts.js
@@ -1,28 +1,33 @@
-/* Yearly farmed animal death totals by species */
-var animals = [
-  { species: 'cows',     deaths: 28740000   },
-  { species: 'chickens', deaths: 8822695000 },
-  { species: 'pigs',     deaths: 115414000  },
-  { species: 'turkeys',  deaths: 232398000  },
-  { species: 'ducks',    deaths: 27794000   },
-  { species: 'lambs',    deaths: 2217000    }
-]
+document.addEventListener('DOMContentLoaded', function() {
 
-/* Calculates today's current total from the yearly total */
-function slaughtersToday( animalDeaths ) {
-  var now = new Date();
-  var secondsToday = now.getSeconds() + (60 * (now.getMinutes() + (60 * now.getHours())));
+  /* Yearly farmed animal death totals by species */
+  var animals = [
+    { species: 'cows',     deaths: 28740000   },
+    { species: 'chickens', deaths: 8822695000 },
+    { species: 'pigs',     deaths: 115414000  },
+    { species: 'turkeys',  deaths: 232398000  },
+    { species: 'ducks',    deaths: 27794000   },
+    { species: 'lambs',    deaths: 2217000    }
+  ]
 
-  return Math.round(( animalDeaths / 365 / 24 / 60 / 60 ) * secondsToday ).toLocaleString();
-}
+  /* Calculates today's current total from the yearly total */
+  function slaughtersToday( animalDeaths ) {
+    var now = new Date();
+    var secondsToday = now.getSeconds() + (60 * (now.getMinutes() + (60 * now.getHours())));
 
-/* Updates the counters */
-function updateCounters() {
-  for ( var i = 0; i < animals.length; i++ ) {
-    var animal = animals[i];
-    var counterElement = document.querySelector( '[data-animal='+animal.species+'] .counter' );
-    counterElement.innerHTML = slaughtersToday( animal.deaths );
+    return Math.round(( animalDeaths / 365 / 24 / 60 / 60 ) * secondsToday ).toLocaleString();
   }
-}
 
-window.setInterval(updateCounters, 1000);
+  /* Updates the counters */
+  function updateCounters() {
+    for ( var i = 0; i < animals.length; i++ ) {
+      var animal = animals[i];
+      var counterElement = document.querySelector( '[data-animal='+animal.species+'] .counter' );
+      counterElement.innerHTML = slaughtersToday( animal.deaths );
+    }
+  }
+
+  updateCounters();
+  window.setInterval(updateCounters, 1000);
+
+});
